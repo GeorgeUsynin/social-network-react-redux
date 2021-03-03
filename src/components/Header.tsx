cb042254f08b72179b5890a49911b0099a69cd21
George Usynin <angiejo03@gmail.com>
2021-03-03 18:53:11 +0300

    commit init

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://www.dlf.pt/png/big/17/170060_photo-logo-png.png"
+                 alt="logo"/>
+        </header>
+    );
+}
+
+export default Header;
