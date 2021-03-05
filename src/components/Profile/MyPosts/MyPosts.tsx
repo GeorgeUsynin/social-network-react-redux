48bebdf8dc0a9b8fc70cd681688c22d828a61fed
George Usynin <angiejo03@gmail.com>
2021-03-05 22:11:05 +0300

    commit create props for Post component (lessson 16)

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -13,8 +13,8 @@ const MyPosts = () => {
                 New posts
             </div>
             <div className={classes.posts}>
-                <Post/>
-                <Post/>
+                <Post message="Hi, my name is George and this is my first social network project" likeCounts={44}/>
+                <Post message="Hi there, I learned how to push props" likeCounts={1}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,22 +1,49 @@
 import React from 'react';
 import classes from "./Post.module.css";
 
+type PostPropsType = {
+    message: string
+    likeCounts: number
+}
 
-const Post = () => {
-    return (
-        <div className={classes.post}>
-            <div className={`${classes.item} ${classes.active}`}>post1</div>
-            <img
-                src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-                alt="woman"/>
-            <div>
+const Post: React.FC<PostPropsType> = (props) => {
+    debugger
+    if (props.likeCounts === 1) {
+        return (
+            <div className={classes.post}>
+                <div className={`${classes.item} ${classes.active}`}>
+                    {props.message}
+                </div>
+                <img
+                    src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                    alt="woman"/>
+                <div>
                     <span>
-                        like
+                        {props.likeCounts} like
                     </span>
+                </div>
             </div>
-        </div>
+        )
+    } else {
+        return (
+            <div className={classes.post}>
+                <div className={`${classes.item} ${classes.active}`}>
+                    {props.message}
+                </div>
+                <img
+                    src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                    alt="woman"/>
+                <div>
+                    <span>
+                        {props.likeCounts} likes
+                    </span>
+                </div>
+            </div>
 
-    );
+        );
+
+    }
+
 }
 
 export default Post;
